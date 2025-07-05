93517c32493bf8b795fa91dd08f522d99290e50e
ohchillman <ohchillman@example.com>
2025-07-05 19:35:54 -0400

    Отключение демо-страницы и установка русского языка по умолчанию

--- packages/frontend/core/src/desktop/router.tsx
+++ packages/frontend/core/src/desktop/router.tsx
@@ -35,7 +35,7 @@ export const topLevelRoutes = [
     children: [
       {
         path: '/',
-        lazy: () => import('./pages/index'),
+        loader: () => redirect('/admin/auth'),
       },
       {
         path: '/workspace/:workspaceId/*',
